0683253bfbc9a2369c4c3a3a9e26ffc7b23bb273
Jamie Lloyd <jamielloyd.work@gmail.com>
2025-05-12 10:12:17 +0200

    Added functions to calculate required pixels and pixel sizing.

--- js/script.js
+++ js/script.js
@@ -1,14 +1,27 @@
-const container = document.querySelector('#grid-wrapper');
-const divNumber = 256;
+const mainContainer = document.querySelector('#grid-wrapper');
+console.log(mainContainer.attributes)
 
+let pixelCount = 16;
+let requiredPixels = pixelCount * pixelCount;
+console.log(requiredPixels);
 
-function calcGrid(pixelNumber) {
+function calcGrid(container, pixels) {
+   let containerHeight = container.offsetHeight;
+   let size = containerHeight / pixels;
+   return size;
+};
+
+let pixelResult = calcGrid(mainContainer, pixelCount);
+console.log(pixelResult);
+
+function formGrid(pixelNumber, pixelSize) {
    for (let i = 0; i < pixelNumber; i++) {
       let pixel = document.createElement('div');
       pixel.classList.add('pixel');
-      pixel.innerHTML = i + 1
-      container.appendChild(pixel);
+      pixel.setAttribute("style", "width:" + pixelSize + "px;height:" + pixelSize + "px;");
+      pixel.innerHTML = i + 1;
+      mainContainer.appendChild(pixel);
    }
 };
 
-calcGrid(divNumber);
+formGrid(requiredPixels, pixelResult);
